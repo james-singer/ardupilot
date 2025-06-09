ccf0d59da00f0338c55b01490ab8ff181a6c1c5a
jd-hanley <joseph.hanley@colorado.edu>
2025-06-09 14:07:30 -0600

    changed gain

--- libraries/AC_AttitudeControl/AC_PosControl.h
+++ libraries/AC_AttitudeControl/AC_PosControl.h
@@ -37,7 +37,7 @@
 
 #define POSCONTROL_RELAX_TC                     0.16f   // This is used to decay the I term to 5% in half a second.
 
-#define STRAIN_TARGET_GAIN 0.002f
+#define STRAIN_TARGET_GAIN 0.0005f
 
 class AC_PosControl
 {
